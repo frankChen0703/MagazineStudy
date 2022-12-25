896adf6c580f32d556dd9ebd2acb8290e32e22bd
frankChen0703 <a070320170415@gmail.com>
2022-12-25 13:33:33 +0800

    12/25

--- hw9/js/buildScene.js
+++ hw9/js/buildScene.js
@@ -9,15 +9,21 @@ function buildScene() {
    var loader = new THREE.TextureLoader();
    var Map = loader.load('https://i.imgur.com/1Wm9iUF.png');
    box = new THREE.Group();
+   material = new THREE.ShadowMaterial();
+   material.opacity = 0.2;
+   
+   scene.add(plane);
    var mat = new THREE.MeshLambertMaterial({
       map: Map
-    });
-   var box1 = new THREE.Mesh(new THREE.ConeGeometry(15, 20, 32), mat);
-   var box2 = new THREE.Mesh(new THREE.CylinderGeometry( 5, 5, 5, 32 ), mat);
+   });
+   var box1 = new THREE.Mesh(new THREE.ConeGeometry(15, 20, 32), mat, material);
+   var box2 = new THREE.Mesh(new THREE.CylinderGeometry(5, 5, 5, 32), mat, material);
    box1.rotation.x = Math.PI;
    box1.position.set(0, 10, 0);
    box2.position.set(0, 22.5, 0);
-   box.add(box1,box2);
+   box.add(box1, box2);
+   box.position.y = -200;
+   box.receiveShadow = true;
    scene.add(box);
 
    text = new MeshText2D("START trun clockwise", {
